d1fe001ae52dbf8a6169637ad71363192a2bc8cc
m4dr1nch <martins.sav@protonmail.com>
2024-11-23 14:14:05 +0200

    Added pipe support for urle & urld.

--- urld/src/main.rs
+++ urld/src/main.rs
@@ -1,10 +1,11 @@
 ///! Decode a URL encoded string
 use std::env;
+use std::io;
 use percent_encoding::percent_decode_str;
 
 fn decode(str_dec: &str) {
     match percent_decode_str(str_dec).decode_utf8() {
-        Ok(out) => println!("{}", out),
+        Ok(out) => print!("{}", out.trim_end()),
         Err(..) => println!("Decode failed!")
     }
 }
@@ -12,14 +13,26 @@ fn decode(str_dec: &str) {
 fn main() {
     let args: Vec<String> = env::args().collect();
     
-    let str_url = match args.get(1) {
-        Some(arg) => arg,
+    match args.get(1) {
+        Some(arg) => {
+            decode(arg);
+        },
         None => {
-            println!("No argument!");
-            return
+            loop {
+                let mut input = String::new();
+                match io::stdin().read_line(&mut input) {
+                    Ok(len) => if len == 0 {
+                        return;
+                    } else {
+                        decode(&input);
+                    }
+                    Err(..) => {
+                        println!("Failed reading STDIN.");
+                        return
+                    }
+                }
+            }
         }
     };
-
-    decode(str_url);
 }
 
--- urle/src/main.rs
+++ urle/src/main.rs
@@ -1,22 +1,35 @@
 ///! Encode a URL encoded string
 use std::env;
+use std::io;
 use percent_encoding::{utf8_percent_encode, NON_ALPHANUMERIC};
 
 fn encode(str_enc: &str) {
     let res = utf8_percent_encode(str_enc, NON_ALPHANUMERIC).to_string();
-    println!("{}", res)
+    print!("{}", res)
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     
-    let str_to_url = match args.get(1) {
-        Some(arg) => arg,
+    match args.get(1) {
+        Some(arg) => {
+            encode(arg);
+        },
         None => {
-            println!("No argument!");
-            return
+            loop {
+                let mut input = String::new();
+                match io::stdin().read_line(&mut input) {
+                    Ok(len) => if len == 0 {
+                        return;
+                    } else {
+                        encode(&input);
+                    }
+                    Err(..) => {
+                        println!("Failed reading STDIN.");
+                        return
+                    }
+                }
+            }
         }
     };
-
-    encode(str_to_url);
 }
